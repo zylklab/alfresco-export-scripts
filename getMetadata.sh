536d55490f1319f97ab9a1a4b450d6fda0043b4f
Cesar Capillas <cesar@zylk.net>
2018-10-31 08:28:13 +0100

    Encoded URLs for webdav

--- getMetadata.sh
+++ getMetadata.sh
@@ -4,6 +4,30 @@
 ### Script for getting Alfresco Metadata files (Bulk Import) for a given downloaded Alfresco folder structure
 ###
 
+urlencode() {
+    # urlencode <string>
+    old_lc_collate=$LC_COLLATE
+    LC_COLLATE=C
+    
+    local length="${#1}"
+    for (( i = 0; i < length; i++ )); do
+        local c="${1:i:1}"
+        case $c in
+            [a-zA-Z0-9.~_-]) printf "$c" ;;
+            *) printf '%%%02X' "'$c" ;;
+        esac
+    done
+    
+    LC_COLLATE=$old_lc_collate
+}
+
+urldecode() {
+    # urldecode <string>
+
+    local url_encoded="${1//+/ }"
+    printf '%b' "${url_encoded//%/\\x}"
+}
+
 # Usage functions
 usage() { echo "Usage: $0 [-f <local-webdav-folder>]" 1>&2; exit 1; }
 
@@ -38,6 +62,10 @@ shift $((OPTIND-1))
 
 # Exports ALFALFURL,MYUSER,MYPASS 
 source ./exportENVARS.sh
+# Default parameters
+#ALFURL=${ALFURL:-http://localhost:8080/alfresco}
+#MYUSER=${MYUSER:-admin}
+#MYPASS=${MYPASS:-admin}
 
 if [ -z "${ALFURL}" ] || [ -z "${MYUSER}" ] || [ -z "${MYPASS}" ]; then
     usage
@@ -48,17 +76,13 @@ if [ -z "${FOLDER}" ]; then
     usage
 fi
 
-# Default parameters
-#ALFURL=${ALFURL:-http://localhost:8080/alfresco}
-#MYUSER=${MYUSER:-admin}
-#MYPASS=${MYPASS:-admin}
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
 for f in `find $FOLDER`
 do
-  aux=`echo "$f" | sed -e 's#^webdav##'`
-  echo "Getting metadata file for path: $aux"
-  curl -s -u $MYUSER:$MYPASS "$ALFURL/service/net/zylk/export-bulk-metadata?path=$aux" > ./webdav/$aux.metadata.properties.xml
+  inp=`urlencode "$f" | sed -e 's#^webdav##'`
+  out=`urldecode "$inp"`
+  echo "Getting metadata file for path: $inp"
+  curl -s -u $MYUSER:$MYPASS "$ALFURL/service/net/zylk/export-bulk-metadata?path=$inp" > "./webdav/$out.metadata.properties.xml"
 done
 IFS=$SAVEIFS
-
