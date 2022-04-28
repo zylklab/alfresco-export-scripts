b28d3a1b0b44b76774910e0d3514eded2e5d60db
[zylk] Cesar Capillas <cesar@zylk.net>
2022-04-28 18:47:04 +0200

    Fixing encoding functions for solving special character path issues

--- getMetadata.sh
+++ getMetadata.sh
@@ -4,6 +4,48 @@
 ### Script for getting Alfresco Metadata files (Bulk Import) for a given downloaded Alfresco folder structure
 ###
 
+rawurlencode() {
+	local string="${1}"
+	local strlen=${#string}
+	local encoded=''
+	local pos c o dot_enc
+	if [ "$2" == 'yes' ]; then
+		dot_enc='yes'
+	fi
+
+	for (( pos=0 ; pos<strlen ; pos++ )); do
+		c=${string:$pos:1}
+		case "$c" in
+			[_~1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-])
+				o="${c}"
+			;;
+			[.])
+				if [[ "$dot_enc" == "yes" ]]; then
+					o=$(printf "$c" | xxd -plain | tr -d '\n' | sed 's/\(..\)/%\1/g')
+				else
+					o="${c}"
+				fi
+			;;
+			*)
+				o=$(printf "$c" | xxd -plain | tr -d '\n' | sed 's/\(..\)/%\1/g')
+		esac
+		encoded+="${o}"
+	done
+
+	echo "$encoded"
+	return 0
+}
+
+rawurldecode() {
+  # This is perhaps a risky gambit, but since all escape characters must be
+  # encoded, we can replace %NN with \xNN and pass the lot to printf -b, which
+  # will decode hex for us
+  printf -v decoded '%b' "${1//%/\\x}"
+
+  echo "${decoded}"
+  return 0
+}
+
 urlencode() {
     # urlencode <string>
     old_lc_collate=$LC_COLLATE
@@ -80,9 +122,15 @@ SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
 for f in `find $FOLDER`
 do
-  inp=`urlencode "$f" | sed -e 's#^webdav##'`
-  out=`urldecode "$inp"`
+
+  #inp=`echo "$f" | sed -e 's#^webdav##'`
+  inp=`rawurlencode "$f" | sed -e 's#^webdav##'`
+  out=`rawurldecode "$inp"`
   echo "Getting metadata file for path: $inp"
-  curl -s -u $MYUSER:$MYPASS "$ALFURL/service/net/zylk/export-bulk-metadata?path=$inp" > "./webdav/$out.metadata.properties.xml"
+  #echo "f   : $f"
+  #echo "inp : $inp"
+  #echo "out : $out"
+  #echo "fld : $f.metadata.properties.xml" 
+  curl -k -s -u $MYUSER:$MYPASS "$ALFURL/service/net/zylk/export-bulk-metadata?path=$inp" > "$f.metadata.properties.xml"
 done
 IFS=$SAVEIFS
